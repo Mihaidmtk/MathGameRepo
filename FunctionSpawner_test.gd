b1af0d0bff448d3b26bd504936c5a5b5b8f08de2
Mihaidmtk <mihai_dumitrescu11@yahoo.com>
2024-05-14 09:11:45 +0300

    muie

--- FunctionSpawner_test.gd
+++ FunctionSpawner_test.gd
@@ -2,16 +2,21 @@ extends Node2D
 @onready var function_spawner = $function_spawner
 @onready var function_spawner_2 = $function_spawner2
 
+var f1 := FunctionSpawner.create()
+var f2 := FunctionSpawner.create() 
+
 var time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	add_child(f1)
+	add_child(f2)
+	f2.position = Vector2(200, 200)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	time += delta
-	function_spawner.update_function(0,time*50)
-	function_spawner_2.update_function(-time*50, 0)
+	f2.update_function(time*100, time*50)
 	pass
---
+++ Scripts/RemadeScripts/Camera2D.gd
@@ -0,0 +1,26 @@
+extends Camera2D
+
+var CAMERA_SPEED := 400
+var direction := Vector2(0,0)
+
+func _ready():
+	
+	limit_left = 0
+	limit_top = 0
+	limit_right = get_parent().cell_count.x * get_parent().cell_size.x + 200
+	limit_bottom = get_parent().cell_count.y * get_parent().cell_size.y + 200
+	
+func _process(delta):
+	direction = Vector2(0,0)
+	if Input.is_action_pressed("move_up"):
+		direction.y = -1
+	elif Input.is_action_pressed("move_down"):
+		direction.y = 1
+		
+	if Input.is_action_pressed("move_right"):
+		direction.x = 1
+	elif Input.is_action_pressed("move_left"):
+		direction.x = -1
+		
+	position += direction * CAMERA_SPEED * delta
+	
