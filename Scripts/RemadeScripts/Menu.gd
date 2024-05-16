05c2f29d75018a38a25f9f367f7a4da846c018a7
Mihaidmtk <mihai_dumitrescu11@yahoo.com>
2024-05-16 19:52:13 +0300

    done2

--- Scripts/RemadeScripts/Menu.gd
+++ Scripts/RemadeScripts/Menu.gd
@@ -7,7 +7,7 @@ func _on_play_pressed():
 
 
 func _on_controls_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/RemadeScenes/UI/info.tscn")
 
 
 func _on_exit_pressed():
